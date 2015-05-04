18249b574dc17c6a2ff8dac44b4e6509e5210e9f
caseymm <casey.miller@voxmedia.com>
2015-05-04 14:03:09 -0400

    should probably include ability to build in config...

--- config.rb
+++ config.rb
@@ -16,3 +16,19 @@ activate :directory_indexes
 
 set :share_url, data.autotune.share_url
 set :tweet_text, data.autotune.tweet_text
+
+
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+  set :http_prefix, data.autotune.base_url if data.autotune.base_url
+end
