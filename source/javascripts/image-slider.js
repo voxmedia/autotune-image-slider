8fe6c9c27a23c5395916d4585ebe185dad091338
caseymm <casey.miller@voxmedia.com>
2015-05-22 11:56:25 -0400

    set timeout will work for now, but needs better solution for pym on refresh

--- source/javascripts/image-slider.js
+++ source/javascripts/image-slider.js
@@ -1,104 +1,104 @@
-var ImageSlider = autotune;
+  var ImageSlider = autotune;
 
-var pymChild = new pym.Child();
+  ImageSlider.Public = ( function ( $ ) {
 
-ImageSlider.Public = ( function ( $ ) {
-  var addImages = function () {
-    // update left photo as background
-    $( '.image-compare-top img' ).each( function () {
-      $( this ).attr( 'src', ImageSlider.image_left.image_url );
-      var parentDiv = $( this ).parent();
-      parentDiv.css( 'background-image', 'url(' + ImageSlider.image_left.image_url + ')' );
-    } );
-    // update right photo as image src
-    $( '.image-compare-bottom img' ).each( function () {
-      $( this ).attr( 'src', ImageSlider.image_right.image_url );
-    } );
-    $( '.image-compare-top' ).addClass(ImageSlider.slider_theme);
+    var addImages = function () {
+      // update left photo as background
+      $( '.image-compare-top img' ).each( function () {
+        $( this ).attr( 'src', ImageSlider.image_left.image_url );
+        var parentDiv = $( this ).parent();
+        parentDiv.css( 'background-image', 'url(' + ImageSlider.image_left.image_url + ')' );
+      } );
+      // update right photo as image src
+      $( '.image-compare-bottom img' ).each( function () {
+        $( this ).attr( 'src', ImageSlider.image_right.image_url );
+      } );
+      $( '.image-compare-top' ).addClass(ImageSlider.slider_theme);
 
-    var left_image = Object.keys(ImageSlider.image_left).length;
-    var right_image = Object.keys(ImageSlider.image_right).length;
+      var left_image = Object.keys(ImageSlider.image_left).length;
+      var right_image = Object.keys(ImageSlider.image_right).length;
 
-    if(left_image > 1 && right_image > 1){
-      ImageSlider.image_source_info = [ImageSlider.image_left, ImageSlider.image_right];
-      addSources('Sources', ImageSlider.image_source_info);
-    } else if (left_image > 1 || right_image > 1){
-      if(left_image > 1){
-        ImageSlider.image_source_info = [ImageSlider.image_left];
-      } else if (right_image > 1){
-        ImageSlider.image_source_info = [ImageSlider.image_right];
+      if(left_image > 1 && right_image > 1){
+        ImageSlider.image_source_info = [ImageSlider.image_left, ImageSlider.image_right];
+        addSources('Sources', ImageSlider.image_source_info);
+      } else if (left_image > 1 || right_image > 1){
+        if(left_image > 1){
+          ImageSlider.image_source_info = [ImageSlider.image_left];
+        } else if (right_image > 1){
+          ImageSlider.image_source_info = [ImageSlider.image_right];
+        }
+        addSources('Source', ImageSlider.image_source_info);
       }
-      addSources('Source', ImageSlider.image_source_info);
+      addEvents();
+    };
+
+    "use strict";
+
+    function addSources(source){
+
+      var source_str = source+": ";
+
+      $.each(ImageSlider.image_source_info, function(i, d){
+        if(d.credit && d['source']){
+          source_str += '<a href="'+d['source']+'" target="blank">'+d.credit+'</a>';
+        } else if(d.credit) {
+          source_str += d.credit;
+        } else if(d['source']) {
+          source_str += '<a href="'+d['source']+'" target="blank">'+d['source']+'</a>';
+        }
+        if(ImageSlider.image_source_info.length > 1){
+          source_str += addSep(i, ImageSlider.image_source_info.length);
+        }
+      })
+      $('.sources').append(source_str);
     }
-    addEvents();
-  };
 
-  "use strict";
-
-  function addSources(source){
-
-    var source_str = source+": ";
-
-    $.each(ImageSlider.image_source_info, function(i, d){
-      if(d.credit && d['source']){
-        source_str += '<a href="'+d['source']+'" target="blank">'+d.credit+'</a>';
-      } else if(d.credit) {
-        source_str += d.credit;
-      } else if(d['source']) {
-        source_str += '<a href="'+d['source']+'" target="blank">'+d['source']+'</a>';
+    function addSep(i, n){
+      if (i==(n-1)){
+        return '';
+      } else if (i==(n-2)){
+        return ' and ';
+      } else {
+        return ', ';
       }
-      if(ImageSlider.image_source_info.length > 1){
-        source_str += addSep(i, ImageSlider.image_source_info.length);
+    }
+
+    var $tool        = $( '.image-compare-tool'       ),
+        $images      = $( '.image-compare-images'     ),
+        $topImage    = $( '.image-compare-top img'    ),
+        $bottomImage = $( '.image-compare-bottom img' );
+
+    var createSlider = function ( e ) {
+      e.preventDefault();
+      var topImage    = $( this ).find( '.image-compare-top' ),
+          bottomImage = $( this ).find( '.image-compare-bottom img' )[0],
+          size        = bottomImage.getBoundingClientRect(),
+          position;
+      if ( e.originalEvent.type == 'mousemove' ) {
+        position    = ( ( e.pageX - size.left ) / bottomImage.offsetWidth ) * 100;
+      } else {
+        position    = ( ( e.originalEvent.touches[0].pageX - size.left ) / bottomImage.offsetWidth ) * 100;
       }
-    })
-    $('.sources').append(source_str);
-    pymChild.sendHeight();
-  }
 
-  function addSep(i, n){
-    if (i==(n-1)){
-      return '';
-    } else if (i==(n-2)){
-      return ' and ';
-    } else {
-      return ', ';
-    }
-  }
+      if ( position <= 100 ) {
+        topImage.css( { 'width': position + '%' } );
+      }
+    };
 
-  var $tool        = $( '.image-compare-tool'       ),
-      $images      = $( '.image-compare-images'     ),
-      $topImage    = $( '.image-compare-top img'    ),
-      $bottomImage = $( '.image-compare-bottom img' );
+    var addEvents = function () {
+      $images.each( function () {
+        $( this ).on( 'mousemove',  createSlider );
+        $( this ).on( 'touchstart', createSlider );
+        $( this ).on( 'touchmove',  createSlider );
+      } );
+    };
 
-  var createSlider = function ( e ) {
-    e.preventDefault();
-    var topImage    = $( this ).find( '.image-compare-top' ),
-        bottomImage = $( this ).find( '.image-compare-bottom img' )[0],
-        size        = bottomImage.getBoundingClientRect(),
-        position;
-    if ( e.originalEvent.type == 'mousemove' ) {
-      position    = ( ( e.pageX - size.left ) / bottomImage.offsetWidth ) * 100;
-    } else {
-      position    = ( ( e.originalEvent.touches[0].pageX - size.left ) / bottomImage.offsetWidth ) * 100;
-    }
-
-    if ( position <= 100 ) {
-      topImage.css( { 'width': position + '%' } );
-    }
-  };
-
-  var addEvents = function () {
-    $images.each( function () {
-      $( this ).on( 'mousemove',  createSlider );
-      $( this ).on( 'touchstart', createSlider );
-      $( this ).on( 'touchmove',  createSlider );
-    } );
-  };
-
-  var init = function () {
+  $(document).ready(function() {
+    var pymChild = new pym.Child();
     addImages();
-  };
+    setTimeout(function(){
+      pymChild.sendHeight();
+    }, 30);
 
-  init();
-
-} ) ( jQuery );
+  });
+})($);
